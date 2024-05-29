330407e1dd4dbdc10e289479a34ea12577352764
George <georgeey123@gmail.com>
2024-05-29 20:54:54 +0000

    add extra fields to support user authentication

--- backend/Domain/Entites/Users.cs
+++ backend/Domain/Entites/Users.cs
@@ -12,7 +12,11 @@ namespace backend.Domain.Entities
         public string LastName { get; set; }
         public string bio { get; set; }
         public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+        public string? VerificationToken { get; set; }
+        public string? PasswordResetToken { get; set; }
+
     }
 }
