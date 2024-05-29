24490482ad9cc80af8161c3ac96752dd7fb4925a
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-05-29 22:13:16 +0000

    Ggpp 19 Design login sign up cards (#4)
    * Created signIn and signOut with testing
    
    * rebased from web
    
    * removed comments

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -15,6 +15,7 @@ const config = {
       center: true,
       padding: "2rem",
       screens: {
+        card: "230px",
         sm: "640px",
         md: "768px",
         lg: "1024px",
@@ -28,6 +29,21 @@ const config = {
       },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
+        primary: "#264FAD",
+        btn: "#264FAD",
+        input: "#EFF3F9",
+        text: "#434343",
+        eye: "#B7B7B7",
+        pops: ["Poppins", "sans-serif", "Arial"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontSize: {
+        tiny: "0.625rem", // 10px
+        huge: "3rem", // 48px
+        giant: "5rem", // 80px
       },
     },
   },
