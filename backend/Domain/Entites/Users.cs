f0ceb53b8a3fa0f1341a8fe9a0a5b1b501ab65c6
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-05-30 20:52:22 +0000

    feat(backend): Ggpp 12 design database schema for users (#2)
    * create base entity model and post post model
    
    * add extra fields to support user authentication
    
    * fix field name formatting

---
+++ backend/Domain/Entites/Users.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Domain.Common;
+
+namespace backend.Domain.Entities
+{
+    public class Users : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+        public string? VerificationToken { get; set; }
+        public string? PasswordResetToken { get; set; }
+
+    }
+}
