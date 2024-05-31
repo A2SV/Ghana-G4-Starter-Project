f138cf36bb2547c18e0cc299fe929ddc4cec5f53
johnteye <johnteye2002@gmail.com>
2024-05-31 20:40:46 +0000

    rebase from web

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -40,6 +40,10 @@ const config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontWeight: {
+        extraThin: "300",
+      },
+
       fontSize: {
         tiny: "0.625rem", // 10px
         huge: "3rem", // 48px
