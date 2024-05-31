ef998f4d42ff5982d40dbf05d22feaaaff579862
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-05-31 20:14:03 +0000

    feat(backend): Ggpp 16 design database schema for posts (#3)
    * add enity model for posts
    
    * fix post class name

---
+++ backend/Domain/Entites/Posts.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Domain.Common;
+
+namespace backend.Domain.Entities
+{
+    public class Posts : BaseEntity
+    {
+        public string Content { get; set; }
+        public string Tag {get; set;}
+
+    }
+}
