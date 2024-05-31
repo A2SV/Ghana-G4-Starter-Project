721a52230648cde5cb4769540bfce4471f814a9d
Boniface Dakey <dak.boniface@gmail.com>
2024-05-31 21:23:11 +0000

    Ggpp 34 set up the theme of the app (#6)
    * fix(mobile): fixing the path
    
    * feat(mobile): added fonts
    
    * feat(mobile): added pacifico font and flutter_svg package
    
    * feat(mobile): added themes and build runner command
    
    * refactor(mobile): removed comments

---
+++ mobile/lib/src/core/theme/theme_config.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:starter_project/src/core/theme/app_light_theme_colors.dart';
+import 'package:starter_project/src/core/theme/text_theme.dart';
+
+class ThemeConfig {
+  static ThemeData get lightTheme {
+    return ThemeData.light().copyWith(
+      textTheme: CustomTextStyles.kDefaultTextTheme(),
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        fillColor: AppLightThemeColors.kFieldBackgroundColor,
+      ),
+      buttonTheme: ButtonThemeData(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
