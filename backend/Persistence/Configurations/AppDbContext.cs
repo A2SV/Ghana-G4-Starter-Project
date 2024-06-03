04e8c6ebba1d16eacd3568c028c332ffe9c70971
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-03 21:51:36 +0000

    feat(backend): Add packages

---
+++ backend/Persistence/Configurations/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public  class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Users>()
+                .HasMany(e=>e.Posts)
+                .WithOne(p=>p.User)
+                .HasForeignKey(e=>e.UserId);
+
+            Seed.UsersSeed.AddSeed(modelBuilder);
+            Seed.PostsSeed.AddSeed(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken=default)
+        { 
+            foreach(var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Added )
+                {
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Posts> Posts { get; set; }
+
+        
+
+    }
+}
