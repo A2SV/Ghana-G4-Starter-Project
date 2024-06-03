391be7302f93a9dfbc3b9f16edff6d06eef95262
codesbyephraim <enamensah011@st.ug.edu.gh>
2024-06-03 22:24:28 +0000

    (fix): fixed file picker error when uploading a file

--- web/app/profile/info/page.tsx
+++ web/app/profile/info/page.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Profile from "@/components/Profile";
 import ProfileInfo from "@/components/ProfileInfo";
+import ProfileNavigation from "@/components/ProfileNavigation";
 import React from "react";
 
 const Info = () => {
   return (
     <div>
       <Header />
-      <Profile />
+      <ProfileNavigation />
       <ProfileInfo />
       <Footer />
     </div>
