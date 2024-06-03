ac82d3d4737d1c929e57781edb3516c9ffb3611e
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-03 15:52:55 +0000

    feat(backend): Implement AppDbContextFactory

---
+++ backend/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class AppDbContextFactory:IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var apiProjectPath = Path.Combine(Directory.GetCurrentDirectory(), "../WebApi");
+            Console.WriteLine(apiProjectPath);
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(apiProjectPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = configuration.GetConnectionString("AppConnectionString");
+            builder.UseNpgsql(connectionString);
+
+            return new AppDbContext(builder.Options);
+        }
+
+        
+    }
+}
