ed9e17c8bae69de89d4cce61b058eb8822058f55
serkhani <dak.boniface@gmail.com>
2024-06-03 16:12:06 +0000

    added custom extensions

---
+++ mobile/lib/src/core/utils/custom_extensions.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+extension CustomStringExtension on String? {
+  Widget asAssetImage({
+    double? size,
+    double? height,
+    double? width,
+    BoxFit fit = BoxFit.cover,
+    Color? color,
+    FilterQuality filterQuality = FilterQuality.low,
+  }) =>
+      Image.asset(
+        this ?? '',
+        height: size ?? height,
+        width: size ?? width,
+        fit: fit,
+        color: color,
+        filterQuality: filterQuality,
+      );
+  Widget asSvgImage({
+    double? size,
+    double? height,
+    double? width,
+    Color? color,
+    BoxFit fit = BoxFit.contain,
+    double? leftPadding,
+    double? rightPadding,
+  }) =>
+      SvgPicture.asset(
+        this ?? '',
+        height: size ?? height,
+        width: size ?? width,
+        fit: fit,
+        placeholderBuilder: (_) => const SizedBox.shrink(),
+      ).leftPadding(leftPadding ?? 0).rightPadding(rightPadding ?? 0);
+}
+
+extension CustomWidgetExtensions on Widget {
+  Widget align(Alignment alignment) => Align(
+        alignment: alignment,
+        child: this,
+      );
+
+  Widget allPadding(double spacing) => Padding(
+        padding: EdgeInsets.all(spacing),
+        child: this,
+      );
+
+  Widget bottomPadding(double spacing) => Padding(
+        padding: EdgeInsets.only(bottom: spacing),
+        child: this,
+      );
+
+  Widget centered() => Center(child: this);
+
+  Widget circularClip(
+    double radius, {
+    Color? color,
+    EdgeInsetsGeometry? padding,
+    BorderRadiusGeometry? borderRadius,
+  }) =>
+      Container(
+        padding: padding,
+        decoration: BoxDecoration(
+          borderRadius: borderRadius ?? BorderRadius.circular(radius),
+          color: color,
+        ),
+        clipBehavior: Clip.hardEdge,
+        child: this,
+      );
+
+  Widget hero({required String tag}) => Hero(
+        tag: tag,
+        child: this,
+      );
+
+  Widget horizontalPadding(double spacing) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: spacing),
+        child: this,
+      );
+
+  Widget leftPadding(double spacing) => Padding(
+        padding: EdgeInsets.only(left: spacing),
+        child: this,
+      );
+
+  Widget onlyPadding(double top, double bottom, double left, double right) =>
+      Padding(
+        padding:
+            EdgeInsets.only(top: top, bottom: bottom, left: left, right: right),
+        child: this,
+      );
+
+  Widget onPressed({
+    required Function()? onTap,
+    BorderRadius? radius,
+    Color? overlayColor,
+    Color? color,
+    InteractiveInkFeatureFactory? splashFactory,
+  }) =>
+      InkWell(
+        onTap: onTap,
+        borderRadius: radius,
+        splashFactory: splashFactory ?? InkSplash.splashFactory,
+        splashColor: color,
+        child: this,
+      );
+
+  Widget onPressedGesture({
+    required Function()? onTap,
+  }) =>
+      GestureDetector(
+        onTap: onTap,
+        child: this,
+      );
+
+  Widget rightPadding(double spacing) => Padding(
+        padding: EdgeInsets.only(right: spacing),
+        child: this,
+      );
+
+  Widget symmetricPadding(double horizontal, double vertical) => Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
+        child: this,
+      );
+
+  Widget topPadding(double spacing) => Padding(
+        padding: EdgeInsets.only(top: spacing),
+        child: this,
+      );
+
+  Widget verticalPadding(double spacing) => Padding(
+        padding: EdgeInsets.symmetric(vertical: spacing),
+        child: this,
+      );
+}
