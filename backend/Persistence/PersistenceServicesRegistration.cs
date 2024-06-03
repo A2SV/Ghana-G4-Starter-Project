54475bd8ec062c1731e0196280c6a659fc17a18e
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-03 15:56:02 +0000

    feat(backend):Register services for Persistence project

---
+++ backend/Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEntityFrameworkNpgsql().
+                AddDbContext<AppDbContext>(options=>
+                options.UseNpgsql(
+                    configuration.GetConnectionString("AppConnectionString"), 
+                    b=>b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+            return services;
+        }
+    }
+}
