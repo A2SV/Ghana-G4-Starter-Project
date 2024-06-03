4cf170a5a9913ce9d60fb5c1302d85b15cf09cb9
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-06-03 23:01:43 +0000

    Design Profile Page My Blogs Section

--- web/test/unit/SignupBackground.test.tsx
+++ web/test/unit/SignupBackground.test.tsx
@@ -1,7 +1,8 @@
 import "@testing-library/jest-dom";
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import SignUpBackground from "@/components/signupBackground";
+import SignUpBackground from "@/components/SignupBackground";
+
 
 describe("SignUpBackground", () => {
   it("renders blog link", () => {
