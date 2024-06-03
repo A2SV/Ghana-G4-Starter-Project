ba8497b11a34a492db8bdd62303667d904cd7679
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-03 15:48:13 +0000

    feat(Program.cs):Register and configure persistence services(DbContext)

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,6 +1,9 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigurePersistenceServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
