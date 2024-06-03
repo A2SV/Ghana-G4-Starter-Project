a923d2d3540d421b8a91a98f09964fff4223daa6
serkhani <dak.boniface@gmail.com>
2024-06-03 18:34:17 +0000

    feat(mobile): add pinput package for OTP input

--- mobile/lib/src/features/auth/presentation/pages/otp_screen.dart
+++ mobile/lib/src/features/auth/presentation/pages/otp_screen.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:starter_project/generated/assets.gen.dart';
 import 'package:starter_project/src/core/utils/custom_extensions.dart';
 import 'package:starter_project/src/core/widgets/custom_button.dart';
+import 'package:starter_project/src/features/auth/presentation/widgets/custom_pinput.dart';
 
-class OtpScreen extends StatelessWidget {
+class OtpScreen extends StatefulWidget {
   static const String routeName = 'otp-screen';
   const OtpScreen({super.key});
+
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  final TextEditingController _otpController = TextEditingController();
+
+  @override
+  void dispose() {
+    _otpController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Assets.svg.amico.path
                 .asSvgImage(
@@ -21,19 +34,27 @@ class OtpScreen extends StatelessWidget {
                   width: 300,
                 )
                 .centered(),
-            const Text(
+            Text(
               'Enter OTP',
-              
+              style: context.textTheme.labelLarge!.copyWith(
+                fontSize: 34.sp,
+              ),
             ),
-            const Text(
+            Text(
               'An OTP has been send to user.name@a2sv.org',
+              style: context.textTheme.labelMedium,
+            ).rightPadding(100.0),
+            CustomPinPutFormField(
+              controller: _otpController,
+              length: 6,
             ),
             CustomButton(
+              horizontalPadding: 0.0,
               text: 'Continue',
               onPressed: () {},
             ),
           ],
-        ),
+        ).horizontalPadding(30.0),
       ),
     );
   }
