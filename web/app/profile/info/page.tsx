8b7ee281a062721df8db52ba62e26f4789381f6d
codesbyephraim <enamensah011@st.ug.edu.gh>
2024-06-03 18:29:29 +0000

    (feat)Desinged Profile page layout and Profile Information page layout

---
+++ web/app/profile/info/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Profile from "@/components/Profile";
+import ProfileInfo from "@/components/ProfileInfo";
+import React from "react";
+
+const Info = () => {
+  return (
+    <div>
+      <Header />
+      <Profile />
+      <ProfileInfo />
+      <Footer />
+    </div>
+  );
+};
+
+export default Info;
