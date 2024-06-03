301ed009a8dcf8f686ba2ae6df7db621ce53a1c0
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-03 17:50:02 +0000

    feat(backend): Add seed for Users for testing

---
+++ backend/Persistence/Seed/UsersSeed.cs
@@ -0,0 +1,48 @@
+﻿using backend.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Seed
+{
+    public static class UsersSeed
+    {
+        public static void AddSeed(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Users>().
+                    HasData(
+                new Users
+                {
+                    Id = 1,
+                    FirstName = "Michael",
+                    LastName = "Adu",
+                    Bio = "Computer Engineering Student",
+                    Username = "mikkayadu",
+                    Email = "michaeladu@gmail.com",
+                    PasswordHash = new byte[]
+            {
+            0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD,
+            0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB,
+            0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF,
+            0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD
+            },
+                    PasswordSalt = new byte[]
+                {
+                0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD,
+            0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB,
+            0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF,
+            0xAB, 0xCD, 0xEF, 0xAB, 0xCD, 0xEF, 0xAB, 0xCD
+
+                },
+
+                    VerificationToken = "akdl8elslejdl",
+                    PasswordResetToken = "None",
+
+                });
+                }
+    }
+}
