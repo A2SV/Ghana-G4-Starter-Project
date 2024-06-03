4b3ea4dabf3aee5b8625acc2ffa156ceda523e21
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-03 17:49:36 +0000

    feat(backend): Add seed for Post for testing

---
+++ backend/Persistence/Seed/PostsSeed.cs
@@ -0,0 +1,27 @@
+﻿using backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Seed
+{
+    public static class PostsSeed
+    {
+        public static void AddSeed(ModelBuilder modelbuilder) {
+            modelbuilder.Entity<Posts>()
+                    .HasData(
+                new Posts
+                {
+                    Id = 1,
+                    Content = "My first Post",
+                    Tag = "Entertainment",
+                    UserId = 1,
+
+
+                });
+        }
+    }
+}
