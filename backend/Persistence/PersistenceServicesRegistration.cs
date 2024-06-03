02f1a24d03b4df968e3cebc878f0923dff4c5fb7
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-03 22:00:37 +0000

    Add reference to Configurations

--- backend/Persistence/PersistenceServicesRegistration.cs
+++ backend/Persistence/PersistenceServicesRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Persistence.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
