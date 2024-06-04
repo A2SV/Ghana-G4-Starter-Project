0bf37c97655e90b3df502dd81b7a035a99b9662d
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2024-06-04 16:54:03 +0000

    feat(mobile): implement the onboarding page (#19)

---
+++ mobile/lib/src/features/on_boarding/presentation/pages/on_boarding3/login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:starter_project/src/features/on_boarding/presentation/pages/on_boarding3/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
