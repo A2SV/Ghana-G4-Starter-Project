85b64d5eaa95b578e6585310b449d296154366bf
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2024-06-04 16:54:03 +0000

    feat(mobile): implement the onboarding page (#19)

---
+++ mobile/lib/src/features/on_boarding/presentation/pages/on_boarding1/on_boarding.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:starter_project/src/features/on_boarding/presentation/pages/on_boarding1/components/body.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
