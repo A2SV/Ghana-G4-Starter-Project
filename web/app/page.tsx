083cbf147ea54fd76f3001e7b3c96816aab5e0d0
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2024-06-05 21:55:32 +0000

    Rebase from web

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,17 +2,12 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import AllPost from "@/components/AllPost";
 import React from "react";
-import CardsContainer from "@/components/CardsContainer";
 
 const DefaultPage = () => {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
       <AllPost />
-=======
-      <CardsContainer />
->>>>>>> e25f3bd ((feat) Created Related blog cards and cards container)
       <Footer />
     </div>
   );
