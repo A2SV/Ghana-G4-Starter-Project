43c6372b619e01eaded1f5de34c31bba8a879e14
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-06-05 22:12:51 +0000

    feat(web): Add shadow to blog's card

--- web/components/ProfileBlogsCard.tsx
+++ web/components/ProfileBlogsCard.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 const ProfileBlogsCard = () => {
   return (
     <div>
-      <div className="md:w-[250px] w-[300px]  flex flex-col space-y-5 pb-[60px]">
+      <div className="md:w-[250px] w-[300px]  flex flex-col space-y-5 pb-[60px] shadow-sm">
         <Image src="/BlogImage.svg" width={250} height={150} alt="Blog Image" />
-        <div className="pt-2">
+        <div className="pt-2 px-[10px]">
           <p className="text-[12px] text-[#5E5873]">Design Liberalized Exchange Rate Management</p>
           <div className="flex flex-row pt-2">
             <Image src="/Avatar.svg" width={20} height={20} className="rounded-lg" alt="avatar" />
@@ -17,11 +17,11 @@ const ProfileBlogsCard = () => {
             <p className="text-[14px] text-[#B9B9C3] pl-2">Jan 10,2020</p>
           </div>
 
-          <div className="flex flex-row pt-2 space-x-6 pl-5">
-            <button className="px-[12px] py-[6px] bg-[#F5F5F5] rounded-md text-[8px] text-[#8E8E8E]">
+          <div className="flex flex-row pt-2 space-x-6  pb-[14px]">
+            <button className="px-[12px] py-[6px] bg-[#F5F5F5] rounded-lg text-[8px] text-[#8E8E8E]">
               UI/UX
             </button>
-            <button className="px-[12px] py-[6px] bg-[#F5F5F5] rounded-md text-[8px] text-[#8E8E8E]">
+            <button className="px-[12px] py-[6px] bg-[#F5F5F5] rounded-lg text-[8px] text-[#8E8E8E]">
               Development
             </button>
           </div>
@@ -29,6 +29,9 @@ const ProfileBlogsCard = () => {
             A little personality goes a long way, especially on a business blog. So don’t be afraid
             to let loose.
           </p>
+          <div className="border-b border-[#EBE9F1] pt-[40px]">
+             
+          </div>
         </div>
       </div>
     </div>
