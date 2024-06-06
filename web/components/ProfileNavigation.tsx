c91489414e263f3e3d8aa9380c0e8b9095e0b089
greatnessmensah <ephraimmensah99@gmail.com>
2024-06-06 20:35:15 +0000

    web(fix): updated link to personal info page

--- web/components/ProfileNavigation.tsx
+++ web/components/ProfileNavigation.tsx
@@ -8,7 +8,7 @@ const ProfileNavigation = () => {
         <h1 className="text-[18px] pb-4 pt-0 font-bold">Profile</h1>
         <div className="flex flex-row space-x-14">
           <h2 className="text-sm">
-            <Link href="/profile/personalinformation">Personal Information</Link>
+            <Link href="/profile/info">Personal Information</Link>
           </h2>
           <h2 className="text-sm text-[#264FAD] border-b-4 pb-3 border-[#264FAD] font-bold">
             <Link href="/profile/myblogs">My Blogs</Link>
