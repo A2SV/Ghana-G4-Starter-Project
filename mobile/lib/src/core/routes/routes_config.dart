4a96a2e43312cae60c65e028a79ab340d6ebb43b
KudusMusah <kudusbanna@gmail.com>
2024-06-06 19:10:46 +0000

    Design profile edit page

--- mobile/lib/src/core/routes/routes_config.dart
+++ mobile/lib/src/core/routes/routes_config.dart
@@ -15,7 +15,7 @@ class AppRouter extends StatelessWidget {
   static GoRouter createRoute() {
     return GoRouter(
       redirect: ((context, state) => redirect(state)),
-      initialLocation: "/all-blogs-screen",
+      initialLocation: "/profile-edit-screen",
       routes: routes,
       observers: [],
     );
