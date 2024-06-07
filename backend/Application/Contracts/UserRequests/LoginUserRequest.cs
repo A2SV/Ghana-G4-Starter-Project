1aa2ce8e8db594c78c47aac719391e5e34d786bf
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-07 22:01:32 +0000

    feat(backend): Remove Username from LoginUserRequest class

--- backend/Application/Contracts/UserRequests/LoginUserRequest.cs
+++ backend/Application/Contracts/UserRequests/LoginUserRequest.cs
@@ -7,6 +7,7 @@ namespace Application.Contracts.UsersRequests
 {
     public class LoginUserRequest
     {
+        
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
