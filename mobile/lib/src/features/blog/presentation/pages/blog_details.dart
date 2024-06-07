bd841df2bae90934d6141ef9b34e31cc5f177273
KudusMusah <kudusbanna@gmail.com>
2024-06-07 09:24:11 +0000

    Fix theme issues

--- mobile/lib/src/features/blog/presentation/pages/blog_details.dart
+++ mobile/lib/src/features/blog/presentation/pages/blog_details.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class BlogDetails extends StatelessWidget {
+  static const String routeName = 'blog-details-screen';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 50,
         backgroundColor: Colors.white,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios_outlined),
@@ -18,33 +21,33 @@ class BlogDetails extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ElevatedButton(
                     onPressed: () {},
-                    child: Text(
-                      'Education',
-                      style: TextStyle(fontSize: 10),
-                    ),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.black54,
                       foregroundColor: Colors.white,
-                      padding: EdgeInsets.symmetric(
+                      padding: const EdgeInsets.symmetric(
                           horizontal: 14, vertical: 6), // Adjust padding
-                      minimumSize: Size(0, 0),
+                      minimumSize: const Size(0, 0),
                       shape: RoundedRectangleBorder(
                         borderRadius:
                             BorderRadius.circular(8), // Adjust border radius
                       ),
                     ),
+                    child: const Text(
+                      'Education',
+                      style: TextStyle(fontSize: 10),
+                    ),
                   ),
-                  Text(
+                  const Text(
                     '50 minutes ago',
                     style: TextStyle(
                       fontSize: 12,
@@ -53,39 +56,50 @@ class BlogDetails extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 8),
-              Text(
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
                 'STUDENTS SHOULD WORK ON IMPROVING THEIR WRITING SKILL',
                 style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
               ),
-              SizedBox(height: 8),
-              Text(
+            ),
+            const SizedBox(height: 8),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
                 'by admin.user@a2sv.org',
                 style: TextStyle(
                   color: Color.fromARGB(255, 17, 12, 12),
                 ),
               ),
-              SizedBox(height: 30),
-              Image.asset(
-                'assets/images/pic1.png',
-                fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 30),
+            Image.asset(
+              'assets/images/pic1.png',
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 23),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 40),
+              child: Text(
+                '    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum...',
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300),
               ),
-              SizedBox(height: 23),
-              Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum...',
-                style: TextStyle(fontSize: 14),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        backgroundColor: Color(0xFF436CC9),
-        child: Icon(Icons.edit, color: Colors.white),
+        backgroundColor: const Color(0xFF436CC9),
+        child: const Icon(Icons.edit, color: Colors.white),
       ),
     );
   }
