1cb2a7411a035269735eb1af34aa24a32bb0b9a6
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2024-06-07 22:29:38 +0000

    Ggpp 42 design card for related posts (#43)
    * (feat) Created Related blog cards and cards container
    
    * Rebase from web
    
    * (feat) Created Related blog cards and cards container
    
    * Rebase from web
    
    * web(feat): added related posts to single blog page
    
    * web(fix): changed relative to absolute imports
    
    ---------
    
    Co-authored-by: greatnessmensah <ephraimmensah99@gmail.com>

--- web/app/blog/single/page.tsx
+++ web/app/blog/single/page.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import SingleBlog from '@/components/SingleBlog'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
+import React from "react";
+import SingleBlog from "@/components/SingleBlog";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import CardsContainer from "@/components/CardsContainer";
 
 const page = () => {
   return (
     <div>
-        <Header />
-        <SingleBlog />
-        <Footer />
+      <Header />
+      <SingleBlog />
+      <CardsContainer />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
