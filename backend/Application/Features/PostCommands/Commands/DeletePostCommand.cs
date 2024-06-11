c589192a09497d22144853cf880fe7b05694cf1f
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-06-11 00:00:20 +0000

    feat(backend): Implement Delete command

---
+++ backend/Application/Features/PostCommands/Commands/DeletePostCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application.Response;
+using Domain.Interfaces;
+using MediatR;
+using Persistence.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.PostCommands.Commands
+{
+    public class DeletePostCommand : IRequest<Result>
+    {
+        public int Id { get; set; }
+    }
+}
