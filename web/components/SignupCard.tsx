5f55812d410786d790f2290cb038eb96a60ac200
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-06-13 22:30:37 +0000

    Ggpp 80 implement functionality for signup (#56)
    * feat(web): finish signup functionality
    
    * feat(web): Implement sign up functionality
    
    * fix(web): resolve PR conversation
    
    * fix(web):fixed errors

--- web/components/SignupCard.tsx
+++ web/components/SignupCard.tsx
@@ -1,41 +1,96 @@
 "use client";
 import React, { useState, FormEvent, ChangeEvent } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { env } from "next-runtime-env";
+import Image from "next/image";
 
 export default function SignupCard() {
-  const [firstName, setFirstName] = useState<string>("");
-  const [lastName, setLastName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("First Name:", firstName);
-    console.log("Last Name:", lastName);
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
+  const router = useRouter();
+
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleFirstNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      firstName: e.target.value,
+    }));
   };
 
   const handleLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      lastName: e.target.value,
+    }));
   };
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      email: e.target.value,
+    }));
   };
 
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      password: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const baseUrl = env("NEXT_PUBLIC_BASE_URL") + "UserAccount/register";
+      await axios.post(baseUrl, user);
+      setMessage("User created successfully!");
+
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+      if (axios.isAxiosError(error)) {
+        console.error("Response:", error.response);
+        console.error("Data:", error.response?.data);
+        console.error("Status:", error.response?.status);
+        setMessage(error.response?.data);
+      } else {
+        setMessage("An error occurred. Please try again later.");
+      }
+    }
+    setLoading(false);
   };
 
   return (
     <div className="h-[600px] w-full flex items-center justify-center ">
-      <div className="w-full ml-[30px] mr-[30px]   md:w-2/4 h-[440px] rounded-xl px-10 py-10 bg-white">
+      <div className="w-full ml-[30px] mr-[30px]   md:w-2/4 h-[500px] rounded-xl px-10 py-10 bg-white">
         <h2 className="text-2xl text-text font-bold mb-3 text-left font-pops">Sign up</h2>
-        <p className="text-sm text-text font-pops mb-2 font-light">
+
+        {message && (
+          <div
+            className="flex items-center bg-red-100  text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <Image
+              src="/ErrorIcon.svg"
+              width={20}
+              height={20}
+              className="rounded-lg"
+              alt="avatar"
+            />
+            <span className="block sm:inline">{message}</span>
+          </div>
+        )}
+
+        <p className="text-sm text-text font-pops mb-2 font-light pt-[16px] pb-[16px]">
           Hey, Enter your details to sign up{" "}
         </p>
         <form onSubmit={handleSubmit}>
@@ -44,7 +99,7 @@ export default function SignupCard() {
               type="text"
               id="first-name"
               className="w-full px-3 py-2  bg-input text-sm mb-4 rounded-md focus:outline-none focus:border-blue-500"
-              value={firstName}
+              value={user.firstName}
               onChange={handleFirstNameChange}
               placeholder="First Name"
               required
@@ -55,7 +110,7 @@ export default function SignupCard() {
               type="text"
               id="last-name"
               className="w-full px-3 py-2  bg-input text-sm mb-4 rounded-md focus:outline-none focus:border-blue-500"
-              value={lastName}
+              value={user.lastName}
               onChange={handleLastNameChange}
               placeholder="Last Name"
               required
@@ -66,7 +121,7 @@ export default function SignupCard() {
               type="email"
               id="email"
               className="w-full px-3 py-2 bg-input text-sm mb-4 rounded-md focus:outline-none focus:border-blue-500"
-              value={email}
+              value={user.email}
               onChange={handleEmailChange}
               placeholder="Email address"
               required
@@ -77,7 +132,7 @@ export default function SignupCard() {
               type="password"
               id="password"
               className="w-full px-3 py-2  bg-input text-sm rounded-md focus:outline-none focus:border-blue-500 mb-7"
-              value={password}
+              value={user.password}
               onChange={handlePasswordChange}
               placeholder="Password"
               required
@@ -87,7 +142,7 @@ export default function SignupCard() {
             type="submit"
             className="w-full bg-primary text-white font-semibold text-sm py-2 font-pops rounded-md hover:bg-blue-600 transition duration-300"
           >
-            Sign up
+            {loading ? "Loading..." : "Sign up"}
           </button>
         </form>
       </div>
