c157d7254b41a20a7408071acc86c7d39c78997a
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-06-13 20:49:38 +0000

    feat(web): finish signup functionality

--- web/components/SignupCard.tsx
+++ web/components/SignupCard.tsx
@@ -1,41 +1,98 @@
 "use client";
 import React, { useState, FormEvent, ChangeEvent } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { env } from "next-runtime-env";
 
 export default function SignupCard() {
-  const [firstName, setFirstName] = useState<string>("");
-  const [lastName, setLastName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  // const [firstName, setFirstName] = useState<string>("");
+  // const [lastName, setLastName] = useState<string>("");
+  // const [email, setEmail] = useState<string>("");
+  // const [password, setPassword] = useState<string>("");
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("First Name:", firstName);
-    console.log("Last Name:", lastName);
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const router = useRouter();
+
+  const [message, setMessage] = useState("");
 
   const handleFirstNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      firstName: e.target.value,
+    }));
   };
 
   const handleLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      lastName: e.target.value,
+    }));
   };
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      email: e.target.value,
+    }));
   };
 
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+    setUser((prevUser) => ({
+      ...prevUser,
+      password: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      console.log("User:", user);
+      const baseUrl = env("NEXT_PUBLIC_BASE_URL") + "UserAccount/register";
+      const response = await axios.post(baseUrl, user);
+      setMessage("User created successfully!");
+      console.log(response.data);
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+      if (axios.isAxiosError(error)) {
+        console.error("Response:", error.response);
+        console.error("Data:", error.response?.data);
+        console.error("Status:", error.response?.status);
+        setMessage(error.response?.data);
+      } else {
+        setMessage("An error occurred. Please try again later.");
+      }
+    }
   };
 
   return (
     <div className="h-[600px] w-full flex items-center justify-center ">
-      <div className="w-full ml-[30px] mr-[30px]   md:w-2/4 h-[440px] rounded-xl px-10 py-10 bg-white">
+      <div className="w-full ml-[30px] mr-[30px]   md:w-2/4 h-[500px] rounded-xl px-10 py-10 bg-white">
         <h2 className="text-2xl text-text font-bold mb-3 text-left font-pops">Sign up</h2>
-        <p className="text-sm text-text font-pops mb-2 font-light">
+
+        {message && (
+          <div
+            className="flex items-center bg-red-100  text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <svg
+              className="fill-current h-6 w-6 text-red-500 mr-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 2C5.58 2 2 5.58 2 10s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm1 11H9v-2h2v2zm0-4H9V5h2v4z" />
+            </svg>
+            <span className="block sm:inline">{message}</span>
+          </div>
+        )}
+
+        <p className="text-sm text-text font-pops mb-2 font-light pt-[16px] pb-[16px]">
           Hey, Enter your details to sign up{" "}
         </p>
         <form onSubmit={handleSubmit}>
@@ -44,7 +101,7 @@ export default function SignupCard() {
               type="text"
               id="first-name"
               className="w-full px-3 py-2  bg-input text-sm mb-4 rounded-md focus:outline-none focus:border-blue-500"
-              value={firstName}
+              value={user.firstName}
               onChange={handleFirstNameChange}
               placeholder="First Name"
               required
@@ -55,7 +112,7 @@ export default function SignupCard() {
               type="text"
               id="last-name"
               className="w-full px-3 py-2  bg-input text-sm mb-4 rounded-md focus:outline-none focus:border-blue-500"
-              value={lastName}
+              value={user.lastName}
               onChange={handleLastNameChange}
               placeholder="Last Name"
               required
@@ -66,7 +123,7 @@ export default function SignupCard() {
               type="email"
               id="email"
               className="w-full px-3 py-2 bg-input text-sm mb-4 rounded-md focus:outline-none focus:border-blue-500"
-              value={email}
+              value={user.email}
               onChange={handleEmailChange}
               placeholder="Email address"
               required
@@ -77,7 +134,7 @@ export default function SignupCard() {
               type="password"
               id="password"
               className="w-full px-3 py-2  bg-input text-sm rounded-md focus:outline-none focus:border-blue-500 mb-7"
-              value={password}
+              value={user.password}
               onChange={handlePasswordChange}
               placeholder="Password"
               required
