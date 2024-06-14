6f9bd330485ab3bb64998051cade27509a2f6e26
Kassimmm <tawfickkassimmohammed@gmail.com>
2024-06-14 01:51:03 +0000

    feat(web): create edit page and modified pages

--- web/components/ProfileBlogs.tsx
+++ web/components/ProfileBlogs.tsx
@@ -7,10 +7,10 @@ import Link from "next/link";
 
 const ProfileBlogs = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <Header />
       <ProfileNavigation />
-      <div className="flex md:flex-row flex-col px-[30px] pt-[30px] justify-between space-y-8">
+      <div className="flex md:flex-row flex-col px-[50px] pt-[30px] justify-between space-y-8">
         <div className="flex flex-col space-y-2">
           <p className="text-[18px] text-[#5D5D5D] ">Manage blogs</p>
           <p className="text-[12px] text-[#868686]">
@@ -27,11 +27,19 @@ const ProfileBlogs = () => {
         </div>
       </div>
 
-      <div className="px-[50px] py-[30px] flex md:flex-row flex-col flex-wrap justify-between mb-[100px] items-center">
-        <ProfileBlogsCard />
-        <ProfileBlogsCard />
-        <ProfileBlogsCard />
-        <ProfileBlogsCard />
+      <div className="px-[50px] py-[30px] flex flex-wrap justify-center mb-[100px]">
+        <div className="w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 p-2">
+          <ProfileBlogsCard />
+        </div>
+        <div className="w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 p-2">
+          <ProfileBlogsCard />
+        </div>
+        <div className="w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 p-2">
+          <ProfileBlogsCard />
+        </div>
+        <div className="w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 p-2">
+          <ProfileBlogsCard />
+        </div>
       </div>
 
       <Footer />
