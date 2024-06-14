250065bc5c261eacfe5482fd84eb44bb6ef14766
Kassimmm <tawfickkassimmohammed@gmail.com>
2024-06-14 21:44:18 +0000

    fix(web): revert changes on profile blod card

--- web/components/ProfileBlogsCard.tsx
+++ web/components/ProfileBlogsCard.tsx
@@ -1,25 +1,26 @@
 "use client";
+import React from "react";
 import Image from "next/image";
-import { FaEdit } from "react-icons/fa";
 import Link from "next/link";
+import { FaEdit } from "react-icons/fa";
 
 const ProfileBlogsCard = () => {
   return (
     <div>
-      <div className="md:w-[300px] w-[300px]  flex flex-col space-y-5 pb-[60px] shadow-sm">
-        <Image src="/BlogImage.svg" width={300} height={150} alt="Blog Image" />
+      <div className="md:w-[250px] w-[300px]  flex flex-col space-y-5 pb-[60px] shadow-sm">
+        <Image src="/BlogImage.svg" width={250} height={150} alt="Blog Image" />
         <div className="pt-2 px-[10px]">
           <p className="text-[12px] text-[#5E5873]">Design Liberalized Exchange Rate Management</p>
           <div className="flex flex-row pt-2">
             <Image src="/Avatar.svg" width={20} height={20} className="rounded-lg" alt="avatar" />
-            <p className="text-[#6262a0] text-[12px] px-2">by</p>
+            <p className="text-[#B9B9C3] text-[12px] px-2">by</p>
             <p className="text-[#6E6B7B] text-[14px]">
               <span>Fred Boone</span> |
             </p>
             <p className="text-[14px] text-[#B9B9C3] pl-2">Jan 10,2020</p>
           </div>
 
-          <div className="flex flex-row pt-2 space-x-6  pb-[12px]">
+          <div className="flex flex-row pt-2 space-x-6  pb-[14px]">
             <button className="px-[12px] py-[6px] bg-[#F5F5F5] rounded-lg text-[8px] text-[#8E8E8E]">
               UI/UX
             </button>
@@ -38,7 +39,6 @@ const ProfileBlogsCard = () => {
               </div>
             </div>
           </Link>
-
           <div className="border-b border-[#EBE9F1] pt-[40px]"></div>
         </div>
       </div>
