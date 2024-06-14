5a5fcbb0048cc65970e9f87f2d9d31050f259a9c
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-06-14 21:22:10 +0000

    (feat): Implement Functionality To Read All Blogs

--- web/redux/store.ts
+++ web/redux/store.ts
@@ -1,14 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { blogApi } from "./blogApi";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { setupListeners } from "@reduxjs/toolkit/query";
+// import { blogApi } from "./blogApi";
 
-export const store = configureStore({
-  reducer: {
-    [blogApi.reducerPath]: blogApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(blogApi.middleware),
-});
+// export const store = configureStore({
+//   reducer: {
+//     [blogApi.reducerPath]: blogApi.reducer,
+//   },
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(blogApi.middleware),
+// });
 
-setupListeners(store.dispatch);
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+// setupListeners(store.dispatch);
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
