b2792a0e338acf494911382d7b58da9395eabbf8
serkhani <dak.boniface@gmail.com>
2024-06-20 10:19:59 +0000

    feat(mobile): implemented blog repository implementation

--- mobile/lib/src/features/blog/data/repositories/blog_repository_impl.dart
+++ mobile/lib/src/features/blog/data/repositories/blog_repository_impl.dart
@@ -1,23 +1,64 @@
 import 'package:dartz/dartz.dart';
-
+import 'package:starter_project/src/core/error/exception.dart';
 import 'package:starter_project/src/core/error/failure.dart';
-
+import 'package:starter_project/src/core/network/network.dart';
+import 'package:starter_project/src/features/blog/data/data_sources/data_sources.dart';
 import 'package:starter_project/src/features/blog/domain/entities/blog.dart';
-
 import 'package:starter_project/src/features/blog/domain/entities/tag.dart';
 import 'package:starter_project/src/features/blog/domain/repositories/repository.dart';
 
+class BlogRepositoryImpl implements BlogRepository {
+  final BlogRemoteDataSource remoteDataSource;
 
-class AuthRepositoryImpl implements BlogRepository {
+  final Network network;
+
+  BlogRepositoryImpl({
+    required this.remoteDataSource,
+    required this.network,
+  });
   @override
-  Future<Either<Failure, Blog>> create({required String title, required String body, required List<Tag> tags}) {
-    // TODO: implement create
-    throw UnimplementedError();
+  Future<Either<Failure, Blog>> create(
+      {required String title,
+      required String body,
+      required List<Tag> tags}) async {
+    if (await network.isConnected) {
+      try {
+        final blog = await remoteDataSource.create(
+          title: title,
+          body: body,
+          tags: tags,
+        );
+        return Right(blog);
+      } on ServerException catch (e) {
+        return Left(
+            ServerFailure(errorMessage: e.errorMessage ?? "Server Error"));
+      }
+    } else {
+      return Left(NetworkFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, Blog>> update({required String id, required String? title, required String? body, required List<Tag>? tags}) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Future<Either<Failure, Blog>> update(
+      {required String id,
+      required String? title,
+      required String? body,
+      required List<Tag>? tags}) async {
+    if (await network.isConnected) {
+      try {
+        final blog = await remoteDataSource.update(
+          id: id,
+          title: title,
+          body: body,
+          tags: tags,
+        );
+        return Right(blog);
+      } on ServerException catch (e) {
+        return Left(
+            ServerFailure(errorMessage: e.errorMessage ?? "Server Error"));
+      }
+    } else {
+      return Left(NetworkFailure());
+    }
   }
 }
