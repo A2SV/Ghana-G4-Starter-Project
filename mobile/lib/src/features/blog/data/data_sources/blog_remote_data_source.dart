e7d674968773562c30fb6a07633452360a2118d4
serkhani <dak.boniface@gmail.com>
2024-06-20 10:14:43 +0000

    feat(mobile): added data sources

--- mobile/lib/src/features/blog/data/data_sources/blog_remote_data_source.dart
+++ mobile/lib/src/features/blog/data/data_sources/blog_remote_data_source.dart
@@ -1,5 +1,69 @@
+import 'dart:convert';
+
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:starter_project/src/core/core.dart';
+import 'package:starter_project/src/core/error/exception.dart';
+import 'package:starter_project/src/features/blog/data/models/blog_model.dart';
+import 'package:starter_project/src/features/blog/domain/domain.dart';
+
 abstract class BlogRemoteDataSource {
-//methods
+  Future<Blog> create({
+    required String title,
+    required String body,
+    required List<Tag> tags,
+  });
+  Future<Blog> update({
+    required String id,
+    required String? title,
+    required String? body,
+    required List<Tag>? tags,
+  });
 }
 
-class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {}
+class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
+  final http.Client client;
+  BlogRemoteDataSourceImpl({required this.client});
+  @override
+  Future<Blog> create(
+      {required String title,
+      required String body,
+      required List<Tag> tags}) async {
+    final token = Hive.box(Constants.authBox).get(Constants.token) ?? "";
+    final response = await client.post(
+      Uri.parse(Constants.createBlogAPIEndpoint),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: json.encode({"title": title, "body": body, "tags": tags}),
+    );
+    if (response.statusCode == 200) {
+      return BlogModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException(errorMessage: response.body);
+    }
+  }
+
+  @override
+  Future<Blog> update(
+      {required String id,
+      required String? title,
+      required String? body,
+      required List<Tag>? tags}) async {
+    final token = Hive.box(Constants.authBox).get(Constants.token) ?? "";
+    final response = await client.post(
+      Uri.parse(Constants.updateBlogAPIEndpoint),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: json.encode({"id": id, "title": title, "body": body, "tags": tags}),
+    );
+    if (response.statusCode == 200) {
+      return BlogModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException(errorMessage: response.body);
+    }
+  }
+}
